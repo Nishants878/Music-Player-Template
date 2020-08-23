a9b8e02af65fbda101fe6ba1b002b1efc35633a8
Nishants878 <nishants878@gmail.com>
2020-08-23 18:04:19 +0530

    Updates

---
+++ precache-manifest.051f1c61b1f77d58bda67e072122331b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e173e965190cc21b21a040110d1bde",
+    "url": "/Music-Player-Template/index.html"
+  },
+  {
+    "revision": "5876475a39fe3aec7fdf",
+    "url": "/Music-Player-Template/static/css/main.235324ba.chunk.css"
+  },
+  {
+    "revision": "a99a61537000475fca65",
+    "url": "/Music-Player-Template/static/js/2.523c34df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Music-Player-Template/static/js/2.523c34df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5876475a39fe3aec7fdf",
+    "url": "/Music-Player-Template/static/js/main.59c768e5.chunk.js"
+  },
+  {
+    "revision": "e0a0e7276b3ecbf19a3e",
+    "url": "/Music-Player-Template/static/js/runtime-main.cf872c41.js"
+  }
+]);
